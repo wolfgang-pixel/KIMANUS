cbe2be6aa54155504f51f9e5c53eb723966544a9
Wolfgang Niermann <wolfgang@niermann.de>
2026-04-11 01:29:30 +0200

    v24.12: Bug-Fix Langdruck startet nicht mehr Action, Tastatur versteckt Bottom-Bar

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // KIMANUS Service Worker v1.0
 // Minimal SW fuer PWA Install-Prompt + Offline-Grundschutz
 
-const CACHE_NAME = 'kimanus-v123';
+const CACHE_NAME = 'kimanus-v124';
 const OFFLINE_URL = '/';
 
 // Install: Startseite cachen fuer Offline-Grundschutz
